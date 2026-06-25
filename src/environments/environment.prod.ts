2bfc33e4aee9793558922f7454212f25dfe18790
JesusArica <220844171+JesusStudio@users.noreply.github.com>
2026-06-24 22:14:04 -0500

    Credenciales habilitadas/ Carpeta environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAtMDYP8RgL4nSspGbt47p0IV0aQmSgyCc",
+    authDomain: "electrohogar-9235c.firebaseapp.com",
+    projectId: "electrohogar-9235c",
+    storageBucket: "electrohogar-9235c.firebasestorage.app",
+    messagingSenderId: "1045411168571",
+    appId: "1:1045411168571:web:4c7dc8a2158d5da0e6771e",
+    measurementId: "G-PW9N6XQP6X"
+  }
+};
